d70af0495fe359d59c2e8b97a47742f05fdfd230
Iain McNulty <iainmcnulty@gmail.com>
2014-07-22 09:41:13 +0100

    Installed devise

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '3.2.18'
 
 gem 'sqlite3'
 
+gem "devise"
+gem 'forem', :github => "radar/forem", branch: "rails3"
+gem 'will_paginate', '3.0.5'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SignalForum::Application.routes.draw do
+  devise_for :users
+
   get "pages/home"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ SignalForum::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
