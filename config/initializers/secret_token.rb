8980f434910b639448c3c311f59280e51cd3fee4
Iain McNulty <iainmcnulty@gmail.com>
2014-07-22 09:15:40 +0100

    Initial Commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SignalForum::Application.config.secret_token = 'e9317c7ccf829b5341ee6bbc40caf8764ed77d9256fc6919b28734d8e158f28747e0425a826ae3702cc5e71024ad877c17d275ff204bfbc80b7429d459e0f03b'
